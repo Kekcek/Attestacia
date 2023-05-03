bdb470eb76415b707c2291c561dff175c5c4ba0c
Киселев Иван <kiseleff2010@yandex.ru>
2023-05-04 00:25:51 +0300

    Создал функцию, которая считает словам с количеством символов <= 3

--- Program.cs
+++ Program.cs
@@ -9,3 +9,22 @@
     Console.WriteLine("");
     return startArray;
 }
+
+int CheckSizeNewArray(string[] startArray)
+{
+    int counter = 0;
+    for (int i = 0; i < startArray.Length; i ++)
+    {
+        if (startArray[i].Length <= 3)
+        {
+            counter++;
+        }
+    }
+
+    return counter;
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+string[] array = CreateStartArray(sizeArray);
+int counter = CheckSizeNewArray(array);
