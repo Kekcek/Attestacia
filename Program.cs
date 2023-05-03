fe2d9354a36058f614b39ad2e7313923a59817f9
Киселев Иван <kiseleff2010@yandex.ru>
2023-05-04 00:27:52 +0300

    Доблавены функцию на проверку слова, перенос слова в новый массив и вывод конечного массива

--- Program.cs
+++ Program.cs
@@ -24,7 +24,43 @@ int CheckSizeNewArray(string[] startArray)
     return counter;
 }
 
+bool CheckSizeWordInLine(string lineArray)
+{
+    if (lineArray.Length <= 3)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+string[] CreateNewArray(string[] startArray, int counter)
+{
+    string[] newArray = new string[counter];
+    int indexArray = 0;
+    for (int i = 0; i < startArray.Length; i++)
+    {
+        if (CheckSizeWordInLine(startArray[i]))
+        {
+            newArray[indexArray] = startArray[i];
+            indexArray++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(string[] newArray)
+{
+   for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.WriteLine(newArray[i]);
+    } 
+Console.WriteLine("");
+}
+
 Console.Write("Введите количество строк в массиве: ");
 int sizeArray = Convert.ToInt32(Console.ReadLine());
 string[] array = CreateStartArray(sizeArray);
 int counter = CheckSizeNewArray(array);
+string[] newArray = CreateNewArray(array, counter);
+PrintArray(newArray);
