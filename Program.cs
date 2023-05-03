aec38f562ebdf5987c0fe77392e83c1c5cdaecb4
Киселев Иван <kiseleff2010@yandex.ru>
2023-05-04 00:23:59 +0300

    Создал функцию, которая заполняет начальный массив

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string[] CreateStartArray(int sizeArray)
+{
+    string[] startArray = new string[sizeArray];
+
+    for (int i = 0; i < sizeArray; i++)
+    {
+        startArray[i] = Console.ReadLine();
+    }
+    Console.WriteLine("");
+    return startArray;
+}
